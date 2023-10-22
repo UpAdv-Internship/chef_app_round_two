530366c1a5e30d6fde4ec22e92de7413d4820ad6
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-10-22 15:38:15 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chef_app_round_two/core/services/service_locator.dart';
 import 'package:chef_app_round_two/features/profile/presentation/cubits/home_cubit/home_cubit.dart';
 import 'package:chef_app_round_two/features/profile/presentation/cubits/update_profile_cubit/update_profile_cubit.dart';
 import 'package:chef_app_round_two/features/sign_in/presentation/cubit/login_cubit.dart';
+import 'package:chef_app_round_two/features/sign_up/presentation/cubit/sign_up_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,7 +18,7 @@ void main() async {
         BlocProvider(create: (context) => sl<UpdateProfileCubit>()),
         BlocProvider(create: (context) => sl<HomeCubit>()..getData()),
         BlocProvider(create: (context) => sl<LoginCubit>()),
-        BlocProvider(create: (context) => sl<LoginCubit>()),
+        BlocProvider(create: (context) => sl<SignUpCubit>()),
       ],
       child: const MyApp(),
     ),
