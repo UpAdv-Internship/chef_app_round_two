bed3b7cdd0b389aedca054940a287c7e1b84a0a3
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-10-22 11:52:16 +0300

    create sign up structure files

---
+++ lib/features/sign_up/presentation/cubit/sign_up_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:chef_app_round_two/features/sign_up/presentation/cubit/sign_up_state.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class SignUpCubit extends Cubit<SignUpState> {
+
+  SignUpCubit() : super(SignUpInitial());
+
+}
+
