6c81fda21af5b94ddd7c198f927304391381397d
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-10-22 17:58:21 +0300

    fix

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -22,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void navigationAfterThreeSeconds() {
     Future.delayed(const Duration(seconds: 3), () {
-      navigateReplacment(context: context, route: Routes.home);
+      navigateReplacment(context: context, route: Routes.login);
     });
 
     //.then((value)async {
